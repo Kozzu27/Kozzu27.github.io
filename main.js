2d21ff59b3db3080ff47b52fbcee5cdb9c9ee2cd
Kozzu27 <143597787+Kozzu27@users.noreply.github.com>
2025-01-08 21:36:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const display = document.getElementById("display");
 
 let numer;
+let historia;
 
 function wyswietl(input){
     display.value += input;
@@ -10,15 +11,34 @@ function wyczysc(){
     display.value = "";
 }
 
+function usun() {
+    numer = display.value;
+    numer = numer.substring(0, numer.length - 1);
+    display.value = numer;
+}
+
+function kopiuj(){
+    var kopiuj = document.getElementById("historia").innerHTML;
+    navigator.clipboard.writeText(kopiuj);
+    document.getElementById('historia').innerHTML = 'Skopiowano';
+    setTimeout(() => {
+        document.getElementById('historia').innerHTML = historia;
+    }, 2000);
+}
+
 function oblicz(){
     try{
+        historia = display.value;
         numer = eval(display.value);
+        historia += " = " + numer;
+        console.log(historia);
         if (Number.isInteger(numer)) {
             display.value = numer;
         } else {
-            display.value = numer.toFixed(7);
+            display.value = numer.toFixed(6);
             console.log(numer);
         }
+        document.getElementById('historia').innerHTML = historia;
     }
     catch(error){
         display.value = "Error";
